727c0450bdaed32c280e873c05caa0c43a27d5d8
Mohan Kumar <33448410+mohankumar17@users.noreply.github.com>
2020-10-23 17:20:56 +0530

    Update motionEstAnalysis.m

--- motionEstAnalysis.m
+++ motionEstAnalysis.m
@@ -25,36 +25,11 @@ for i = 0:30
     elseif imgPNumber < 100
         imgPFile = sprintf('./%s/%s0%d.%s',folder_name,imageName, imgPNumber, image_format);
     end
-    
-%         if img_ref_frm_num < 10
-%         img_ref_frm_file = sprintf('./%s/%s00%d.%s',folder_name, image_name, img_ref_frm_num,image_format);
-%     elseif img_ref_frm_num < 100
-%         img_ref_frm_file = sprintf('./%s/%s0%d.%s',folder_name, image_name, img_ref_frm_num,image_format);
-%     end
-% 
-%     if img_curr_frm_num < 10
-%         img_cur_frm_file = sprintf('./%s/%s00%d.%s',folder_name, image_name, img_curr_frm_num,image_format);
-%     elseif img_curr_frm_num < 100
-%         img_cur_frm_file = sprintf('./%s/%s0%d.%s',folder_name, image_name, img_curr_frm_num,image_format);
-%     end
 
     imgI = double(imread(imgIFile));
     imgP = double(imread(imgPFile));
     imgI = imgI(:,1:352);
     imgP = imgP(:,1:352);
-    
-    % Exhaustive Search
-    [motionVect, computations] = motionEstES(imgP,imgI,mbSize,p);
-    toc
-    imgComp = motionComp(imgI, motionVect, mbSize);
-    ESpsnr(i+1) = imgPSNR(imgP, imgComp, 255);          
-    EScomputations(i+1) = computations;
-end
-
-fprintf('Average PSNR: %f\n',(mean(ESpsnr)));
-fprintf('Average EScomputations: %f\n',(sum(EScomputations)));
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
 
  % Cross Hexagonal Search
     [motionVect, computations] = motionEstNHS(imgP,imgI,mbSize,p);
@@ -65,6 +40,6 @@ fprintf('Average EScomputations: %f\n',(sum(EScomputations)));
 end
 
 fprintf('Average PSNR: %f\n',(mean(NHSpsnr)));
-fprintf('Average EScomputations: %f\n',(sum(NHScomputations)));
+fprintf('Average computations: %f\n',(sum(NHScomputations)));
 
 
